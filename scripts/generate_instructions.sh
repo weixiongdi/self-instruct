77618f4e5a8d02b6834f0a19d9abd83663b1fee0
weixiongdi <32905378+weixiongdi@users.noreply.github.com>
2023-07-07 16:31:33 +0800

    Update generate_instructions.sh

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -4,4 +4,5 @@ python self_instruct/bootstrap_instructions.py \
     --batch_dir ${batch_dir} \
     --num_instructions_to_generate 50000 \
     --seed_tasks_path data/seed_tasks.jsonl \
-    --engine "davinci"
+    --engine "davinci" \
+    --api_key "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"
